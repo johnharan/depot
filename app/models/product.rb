4a2c18c3e82c86570292bb712961e8d4ef6fd302
johnharan <johnharan@users.noreply.github.com>
2014-10-01 12:34:34 +0100

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ActiveRecord::Base
   
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   
 end
