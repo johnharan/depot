4650045790b02e18a8fe475d6d44499b42475ffa
HARAN <11452978@brn003-linux.(none)>
2014-11-12 14:43:05 +0000

    14 done

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,17 @@
 class Order < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
+        validates :title, :description, :image_url, presence: true, allow_blank: false
+
+	PAYMENT_TYPES = [ "Cheque", "Credit card", "Purchase order" ]
+
+        validates :pay_type, inclusion: PAYMENT_TYPES
+
+        def add_line_items_from_cart(cart)
+  	   cart.line_items.each do |item|
+    	     item.cart_id = nil
+             line_items << item
+           end
+        end
+
+	
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -26,7 +26,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
@@ -52,9 +52,9 @@ class OrdersControllerTest < ActionController::TestCase
   test "requires item in cart" do
     get :new
     assert_redirected_to store_path
-    assert_equal flash[:notice] 'Your cart is empty'
+    assert_equal flash[:notice], 'Your cart is empty'
   end
 
     assert_redirected_to orders_path
-  end
+ end
 end
